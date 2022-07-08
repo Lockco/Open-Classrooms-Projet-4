41db31370488e5276d22b8cbcf97f5fecb57470b
Loïc Ruffiot <loicruffiot@gmail.com>
2022-07-08 09:03:23 +0200

    update

---
+++ models/tournament.py
@@ -0,0 +1,14 @@
+class tournament:
+    
+    def __init__(self, players='', tours=[], name='', place ='', date='', number_of_turns=[], time = ['bullet', 'blitz', 'coup rapide'], description=''):
+
+        self.players = players
+        self.tours = tours
+        self.name = name
+        self.place = place
+        self.date = date
+        self.number_of_turns = number_of_turns
+        self.time = time
+        self.description = description
+
+    print('tournois créé. ')
