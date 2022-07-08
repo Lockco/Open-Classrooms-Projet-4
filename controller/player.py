8b2ef675d4734527a4cc9eb3fd5afa8a9d5d4cb2
Loïc Ruffiot <loicruffiot@gmail.com>
2022-07-08 10:57:32 +0200

    update

--- controller/player.py
+++ controller/player.py
@@ -1,7 +1,13 @@
 '''fichier responsable de la gestion des fonctionnalité lié à la création des joueurs'''
 
 def create_player():
+    '''Création d'un joueur'''
+    
+    ''''''
+
     pass
 
 def ranking():
-    pass
+   '''fonction qui permettra '''
+
+   pass
--- models/match.py
+++ models/match.py
@@ -2,4 +2,7 @@ class Match:
 
     def __init__(self, name, players_pair):
 
+        self.name = name
+        self.players_pair = players_pair
+        
         pass
